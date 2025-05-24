efb39bf366a3fb2d07c53c8da6a1bc6afd04b623
ThePixelbrain <19214217+ThePixelbrain@users.noreply.github.com>
2025-05-24 12:17:49 +0200

    Load FunkyLocomotion Mixin on a more generic jar name

--- src/main/java/uk/bedcraft/bedcraftfixes/mixinplugin/TargetedMod.java
+++ src/main/java/uk/bedcraft/bedcraftfixes/mixinplugin/TargetedMod.java
@@ -16,7 +16,7 @@ public enum TargetedMod {
     // Replace with your injected mods here, but always keep VANILLA:
     VANILLA("Minecraft", "unused", true),
     FTB_UTILITIES("FTBUtilities", "FTBUtilities", true),
-    FUNKY_LOCOMOTION("FunkyLocomotion", "FunkyLocomotion", true),
+    FUNKY_LOCOMOTION("FunkyLocomotion", "Funky", true),
     HQM("HQM", "HQM", true);
 
     public final String modName;
